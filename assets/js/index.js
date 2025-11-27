9d5043ead430bfe6ba7f7a89fb93d1ff849f12e0
DevMallone <kaull.dev@gmail.com>
2025-11-26 23:54:03 -0300

    Search filter completed, catalog banner finalized, and product items changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,51 @@
+let allProducts = [];
 // JSON CATALOG
 fetch("assets/js/catalog.json")
   .then((response) => response.json())
   .then((products) => {
-    // PRINT PRODUCTS
-    products.forEach((product) => {
-      let boxProducts = document.querySelector(".boxProducts");
-      boxProducts.innerHTML += `
-        <li>  
-            <a href='#'>
-                <i class='fi-tc-cart-minus'></i>
-                <figure>
-                    <img src='${product.img}' alt='${product.name}'>
-                </figure>
-                <h3>${product.name}</h3>
-                <h6>R$ ${product.price}</h6>       
-            </a>
-        </li>
-      `;
-    });
+    allProducts = products;
+    printProducts(allProducts);
   });
+
+// PRINT FUNCTION PRODUCTS
+function printProducts(list) {
+  let boxProducts = document.querySelector(".products-box");
+  boxProducts.innerHTML = "";
+
+  list.forEach((product) => {
+    let price = product.price.toFixed(2).replace(".", ",");
+
+    boxProducts.innerHTML += `
+    <li>  
+      <i class='fi-tc-plus' title='Adicionar ao Carrinho'></i>
+      <figure>
+        <img src='${product.img}' alt='${product.name}'>
+      </figure>
+      <div>
+        <h3>${product.name}</h3>
+        <h6><em>R$</em> ${price}</h6>
+      </div>  
+    </li>
+    `;
+  });
+}
+
+// SEARCH ITENS
+document.getElementById("search").addEventListener("input", () => {
+  let text = search.value.toLowerCase();
+  let noProduct = document.querySelector(".no-product");
+  let filtered = allProducts.filter((product) =>
+    product.name.toLowerCase().includes(text)
+  );
+
+  if (filtered.length === 0) {
+    document.querySelector(".products-box").innerHTML = "";
+    noProduct.style.display = "flex";
+    // get value product
+    let nameNoIdentify = document.querySelector(".no-product em");
+    nameNoIdentify.innerHTML = search.value;
+    return;
+  }
+  noProduct.style.display = "none";
+  printProducts(filtered);
+});
